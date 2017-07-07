f9d01d36c471d88e89e041fcff05662fd326b7ce
akil udayakumar <akil@everycrave.me>
2017-07-07 19:06:27 +0530

    fixed ui not mobile responsive

--- codeville/views.py
+++ codeville/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from .models import UserDetail
+from django.core.mail import EmailMessage
 
 # Create your views here.
 def create_user(request):
@@ -9,13 +10,26 @@ def create_user(request):
 def contributors(request):
     return render(request, 'codeville/contrib.html',{})
 
+
+# def send_email(recipient):
+#     body = '''
+
+#     Hello,
+
+#     Thank you for registering on codeville, to complete your registration, please come to the office on Monday between 10 to 11 am.
+
+#      '''
+#     email = EmailMessage('Codeville - Complete your registration', body, to=[recipient])
+#     email.send()   
+
 @csrf_exempt
 def add(request):
     print("In add")
     name=request.POST['name']
     email=request.POST['email']
     phone=request.POST['phone']
-    phone="+91"+phone
+    phone="91"+phone
+    # send_email(email)
     try:
         user = UserDetail(name=name,email=email,phone=phone)
         print(user)
