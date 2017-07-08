b6b8585c133a7380f0d9dfec911cab3782f4e52c
akil udayakumar <akil@everycrave.me>
2017-07-08 12:31:12 +0530

    nuked the repo

--- codeville/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.create_user, name='create_user'),
-    url(r'^contributors$', views.contributors, name='contributors'),
-    url(r'add', views.add, name="add"),
-]
